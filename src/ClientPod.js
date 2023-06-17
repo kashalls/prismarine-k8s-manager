60217f173e8ea9b57be536d8f3082ed82b1c3d61
Kashalls <kashalls@pm.me>
2023-06-17 15:54:19 -0700

    jesus

--- src/ClientPod.js
+++ src/ClientPod.js
@@ -23,7 +23,7 @@ export default class ClientPod {
         containers: [
           {
             name: this.namespace,
-            image: "ghcr.io/kashalls/prismarine-k8s-runner:main-ed9d0408-1687033039",
+            image: "ghcr.io/kashalls/prismarine-k8s-runner:main-5f477711-1687042395",
             env: this.env,
           },
         ],
