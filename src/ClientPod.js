424dc0b15b5f0307b1e68a64db87247139b91c5a
Kashalls <kashalls@pm.me>
2023-06-18 12:55:09 -0700

    yay

--- src/ClientPod.js
+++ src/ClientPod.js
@@ -23,11 +23,20 @@ export default class ClientPod {
         containers: [
           {
             name: this.namespace,
-            image: "ghcr.io/kashalls/prismarine-k8s-runner:main-e807c5cc-1687070533",
+            image: "ghcr.io/kashalls/prismarine-k8s-runner:main-be377b8c-1687117387",
             env: [
               {
                 name: 'MINEFLAYER_CONFIG',
                 value: JSON.stringify(this.options)
+              },
+              {
+                name: 'CONTROLLER_HOST',
+                value: process.env.PRISMARINE_CONTROLLER_SERVICE_HOST
+              },
+              ,
+              {
+                name: 'CONTROLLER_PORT',
+                value: process.env.PRISMARINE_CONTROLLER_SERVICE_PORT
               }
             ]
           },
