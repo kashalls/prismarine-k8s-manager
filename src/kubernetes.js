2dbfe60ad3e57d237d5563895bfbdc8f55d8ef65
Kashalls <kashalls@pm.me>
2023-07-25 11:03:59 -0700

    chore: add k8s networking api

--- src/kubernetes.js
+++ src/kubernetes.js
@@ -7,6 +7,7 @@ export const Apps = config.makeApiClient(k8s.AppsV1Api);
 export const CoreApi = config.makeApiClient(k8s.CoreV1Api);
 export const Custom = config.makeApiClient(k8s.CustomObjectsApi);
 export const Core = config.makeApiClient(k8s.CoreApi);
+export const NetworkingApi = config.makeApiClient(k8s.NetworkingV1Api)
 
 export const Watcher = new k8s.Watch(config);
 
