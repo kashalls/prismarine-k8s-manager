fbb8da781d9ad8ef3a198449dc5938a76cadab04
Kashalls <kashalls@pm.me>
2023-07-25 12:41:54 -0700

    chore: use unified topology

--- src/mongodb.js
+++ src/mongodb.js
@@ -1,3 +1,3 @@
 import { MongoClient } from 'mongodb'
 
-export default new MongoClient(process.env.MONGO_CONNECTION_STRING)
+export default new MongoClient(process.env.MONGO_CONNECTION_STRING, { useUnifiedTopology: true }-)
