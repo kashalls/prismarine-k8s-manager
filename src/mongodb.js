b8607a1d16074aef655b306d08257410c6da1861
Kashalls <kashalls@pm.me>
2023-07-25 13:23:19 -0700

    aaaaaaaaaaaa

--- src/mongodb.js
+++ src/mongodb.js
@@ -1,2 +1,2 @@
 import { MongoClient } from 'mongodb'
-export default new MongoClient(process.env.MONGO_CONNECTION_STRING, { useUnifiedTopology: true })
+export default new MongoClient(process.env.MONGO_CONNECTION_STRING)
